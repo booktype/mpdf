054df116be7bd4b4ecde778dbf9f8588ea88e7c5
Aleksandar Erkalovic <aerkalov@gmail.com>
2016-06-08 18:03:51 +0200

    Initial upload

--- config_fonts.php
+++ config_fonts.php
@@ -81,6 +81,72 @@ $this->fonttrans = array(
  */
 
 $this->fontdata = array(
+        /* theme fonts */
+        "roboto" => array(
+          'R' => "Roboto-Regular.ttf",
+          'B' => "Roboto-Bold.ttf",
+          'I' => "Roboto-Italic.ttf",
+          'BI' => "Roboto-BoldItalic.ttf",
+        ),
+        "roboto-condensed" => array(
+          'R' => "RobotoCondensed-Regular.ttf",
+          'B' => "RobotoCondensed-Bold.ttf",
+          'I' => "RobotoCondensed-Italic.ttf",
+          'BI' => "RobotoCondensed-BoldItalic.ttf",
+        ),
+        "roboto-light" => array(
+          'R' => "Roboto-Light.ttf",
+          'B' => "Roboto-Regular.ttf",
+          'I' => "Roboto-LightItalic.ttf",
+          'BI' => "Roboto-Italic.ttf",
+        ),
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
+        "texgyreadventor" => array(
+          'R' => "TeXGyreAdventor-Regular.ttf",
+          'B' => "TeXGyreAdventor-Bold.ttf",
+          'I' => "TeXGyreAdventor-Italic.ttf",
+          'BI' => "TeXGyreAdventor-BoldItalic.ttf",
+        ),
+        "texgyreschola" => array(
+          'R' => "TeXGyreSchola-Regular.ttf",
+          'B' => "TeXGyreSchola-Bold.ttf",
+          'I' => "TeXGyreSchola-Italic.ttf",
+          'BI' => "TeXGyreSchola-BoldItalic.ttf",
+        ),
+        "freemono" => array(
+          'R' => "FreeMono.ttf",
+          'B' => "FreeMonoBold.ttf",
+          'I' => "FreeMonoOblique.ttf",
+          'BI' => "FreeMonoBoldOblique.ttf",
+        ),
+        "ubuntu" => array(
+          'R' => "Ubuntu-R.ttf",
+          'B' => "Ubuntu-B.ttf",
+          'I' => "Ubuntu-RI.ttf",
+          'BI' => "Ubuntu-BI.ttf",
+        ),
+        "ubuntu-mono" => array(
+          'R' => "UbuntuMono-R.ttf",
+          'B' => "UbuntuMono-B.ttf",
+          'I' => "UbuntuMono-RI.ttf",
+          'BI' => "UbuntuMono-BI.ttf",
+        ),
+        "ubuntu-light" => array(
+          'R' => "Ubuntu-L.ttf",
+          'B' => "Ubuntu-M.ttf",
+          'I' => "Ubuntu-LI.ttf",
+          'BI' => "Ubuntu-MI.ttf",
+        ),
+        "im_fell_dw_pica_pro" => array(
+          'R' => "IM_FELL_DW_Pica_PRO_Roman.ttf",
+          'I' => "IM_FELL_DW_Pica_PRO_Italic.ttf",
+        ),
+        /* core fonts */
 	"dejavusanscondensed" => array(
 		'R' => "DejaVuSansCondensed.ttf",
 		'B' => "DejaVuSansCondensed-Bold.ttf",
